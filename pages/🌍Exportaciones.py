2527abb57cf83ff605c6955c2047f696b8612911
derquicia <derquicia@mariogoldsteinsa.com.ar>
2024-11-14 14:51:50 -0300

    Update 🌍Exportaciones.py

--- pages/🌍Exportaciones.py
+++ pages/🌍Exportaciones.py
@@ -81,7 +81,7 @@ conn = st.connection("postgresql", type="sql")
 dfp = conn.query('select anio,pais, value,fob from info_expo_anio_paises ;', ttl="0")
 #st.write(dfp['pais'])
 #json_list = json.loads(json.dumps(list(dfp.T.to_dict().values()))) 
-tt = "[[""anio"",""pais"",""value"",""fob""],["
+tt = "[["""anio""","""pais""","""value""","""fob"""],["
 st.write(tt)
 f = dfp.to_json(orient="values")
 
