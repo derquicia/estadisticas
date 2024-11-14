327ddde2878556aa33205e72d769e5858936c1dd
derquicia <derquicia@mariogoldsteinsa.com.ar>
2024-11-14 12:17:36 -0300

    Update 🌍Exportaciones.py

--- pages/🌍Exportaciones.py
+++ pages/🌍Exportaciones.py
@@ -79,7 +79,7 @@ st_echarts(
 
 c = (
        Line()
-       .add_xaxis(df['anio])
+       .add_xaxis(df['anio'])
        .add_yaxis("Litros", df['litros])
        .add_yaxis("fob", df[fob])
        .set_global_opts(title_opts=opts.TitleOpts(title="titulo"))
