4375d9f49f31f62df97e533df3d33ac6a57522f6
derquicia <derquicia@mariogoldsteinsa.com.ar>
2024-11-14 13:25:23 -0300

    Update 🌍Exportaciones.py

--- pages/🌍Exportaciones.py
+++ pages/🌍Exportaciones.py
@@ -79,10 +79,10 @@ st_echarts(
 
 conn = st.connection("postgresql", type="sql")
 dfp = conn.query('select anio,pais, value,fob from info_expo_anio_paises ;', ttl="0")
-#st.write(df)
+st.write(dfp)
 json_list = json.loads(json.dumps(list(dfp.T.to_dict().values()))) 
 raw_data = json_list
-
+st.write(json_list)
 countries = [
         "DINAMARCA",
         "ESTADOS UNIDOS",
