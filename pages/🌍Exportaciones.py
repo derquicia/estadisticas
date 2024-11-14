ab11b87987de7823e93a666cb475c969e250446c
derquicia <derquicia@mariogoldsteinsa.com.ar>
2024-11-14 17:04:26 -0300

    Update 🌍Exportaciones.py

--- pages/🌍Exportaciones.py
+++ pages/🌍Exportaciones.py
@@ -100,7 +100,7 @@ st.write(raw_data)
 
 with open("./data/life-expectancy-table.json") as f:
         raw_data1 = json.load(f)
-st.write(f)  
+st.write(dfpe['country'].to_list())  
 
 countries = [
         "Finland",
