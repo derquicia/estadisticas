f5e31185cd7ca01d5318c17d23a69fd2a22a9d68
derquicia <derquicia@mariogoldsteinsa.com.ar>
2024-11-15 15:04:15 -0300

    Update estadisticas.py

--- estadisticas.py
+++ estadisticas.py
@@ -122,6 +122,19 @@ with col[1]:
 
     st.markdown('Por Envases Vinos Tintos')
     options = {
+        "color": [
+        '#dd6b66',
+        '#759aa0',
+        '#e69d87',
+        '#8dc1a9',
+        '#ea7e53',
+        '#eedd78',
+        '#73a373',
+        '#73b9bc',
+        '#7289ab',
+        '#91ca8c',
+        '#f49f42'
+         ]
         "title": {"text": "", "left": "center"},
         "subtitle":{"text": ""},
         "tooltip": {"trigger": "item"},
