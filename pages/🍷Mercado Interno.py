4fa3a4730449ca926993f3f0e5f2a903358d81d4
derquicia <derquicia@mariogoldsteinsa.com.ar>
2024-11-15 17:01:35 -0300

    Update 🍷Mercado Interno.py

--- pages/🍷Mercado Interno.py
+++ pages/🍷Mercado Interno.py
@@ -46,7 +46,7 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 
 conn = st.connection("postgresql", type="sql")
-dfd = conn.query('select anio,tintos,blancos,rosados from info_desp_mat_anio;', ttl="0"),
+dfd = conn.query('select anio,tintos,blancos,rosados from info_desp_anio_v1;', ttl="0"),
 st.write(dfd[0])
 df = dfd[0]
  
@@ -74,6 +74,42 @@ option = {
     "series": [{"data": df['tintos'].to_list(), "type": "line", "name": 'Tintos'},
                {"data": df['blancos'].to_list(), "type": "line", "name": 'Blancos'},
                {"data": df['rosados'].to_list(), "type": "line", "name": 'Rosados'},
+               ]
+}
+st_echarts(
+    options=option, height="400px" ,
+)
+
+
+conn = st.connection("postgresql", type="sql")
+df1 = conn.query('select anio,tintos,blancos,rosados from info_desp_anio_mes_v1;', ttl="0"),
+st.write(df1[0])
+df2 = df1[0]
+ 
+st.subheader('Evolución de los despachos por Mes')
+
+if st.checkbox('Ver datos en forma de tabla'):
+    st.write(df)
+
+
+#dfd['anio'] = dfd['anio'].astype(str)
+
+#newdf=dfd.set_index('anio',inplace=False).rename_axis(None)
+
+option = {
+    "tooltip": {
+        "trigger": 'axis',
+        "axisPointer": { "type": 'cross' }
+    },
+    "legend": {},    
+    "xAxis": {
+        "type": "category",
+        "data": df2['anio'].to_list(),
+    },
+    "yAxis": {"type": "value"},
+    "series": [{"data": df2['tintos'].to_list(), "type": "line", "name": 'Tintos'},
+               {"data": df2['blancos'].to_list(), "type": "line", "name": 'Blancos'},
+               {"data": df2['rosados'].to_list(), "type": "line", "name": 'Rosados'},
                ]
 }
 st_echarts(
