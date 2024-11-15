d1aa05cd27aadd886fcc40f02412ac7e4f61e3f2
derquicia <derquicia@mariogoldsteinsa.com.ar>
2024-11-15 16:51:57 -0300

    Update 🍷Mercado Interno.py

--- pages/🍷Mercado Interno.py
+++ pages/🍷Mercado Interno.py
@@ -47,7 +47,7 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 conn = st.connection("postgresql", type="sql")
 dfd = conn.query('select anio,tintos,blancos,rosados from info_desp_mat_anio;', ttl="0"),
-st.write(dfd['anio'])
+st.write(dfd[0])
  
 st.subheader('Evolución de los despachos por año')
 
